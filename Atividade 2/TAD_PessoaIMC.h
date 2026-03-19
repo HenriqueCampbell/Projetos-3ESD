b7cb708c204aed1835ef17fa1a63b969ea1cced8
Henrique Campbell <hctrechau@gmail.com>
2026-03-19 19:09:18 -0300

    feat finalizado TAD_PessoaIMC.h

--- Atividade 2/TAD_PessoaIMC.h
+++ Atividade 2/TAD_PessoaIMC.h
@@ -1,16 +1,22 @@
-// Nome: Matheus Peixoto Menezes
-// Nome: Manuelle Felix da Silva
-
 #include "TAD_Altura.h"
 #include "TAD_Peso.h"
 
+// Nome: Matheus Peixoto Menezes
+// Nome: Manuelle Felix da Silva
+
 /* Tipo exportado */
 /* cria a medida do Indice de Massa Corporal */
-typedef struct imc IMC;
+typedef struct pessoaIMC IMC;
 
 /* Funções exportadas */
 /* Função cria - Aloca e retorna um IMC (altura e peso) */
-IMC *imc_cria(Altura *alt, Peso *peso);
+IMC *imc_cria(char *nome, Altura *alt, Peso *peso);
+
+/* Função altera peso - Altera o peso de um IMC */
+void imc_alteraPeso(IMC *imc, Peso *g);
+
+/* Função altera altura - Altera a altura de um IMC */
+void imc_alteraAltura(IMC *imc, Altura *cm);
 
 /* Função libera - Libera a memória de um IMC previamente criado */
 void imc_libera(IMC *imc);
@@ -26,6 +32,10 @@ char *imc_getIMC(IMC *imc);
 
 int imc_compara(IMC *p1, IMC *p2);
 
-IMC *imc_soma(IMC *p, IMC *p2);
-
 IMC *imc_copia(IMC *p);
+
+/* Função Calcula IMC - Calcula o Índice de Massa Corporal */
+float imc_calculaIMC(IMC *p);
+
+/* Função categorizar - Categoriza o IMC */
+char *imc_categorizarIMC(IMC *p);
