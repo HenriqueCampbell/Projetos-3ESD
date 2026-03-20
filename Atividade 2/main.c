13fbc5962a5c1533c3332c53cccbc7bba11bc04d
Henrique Campbell <hctrechau@gmail.com>
2026-03-19 21:19:24 -0300

    bug fix main

--- Atividade 2/main.c
+++ Atividade 2/main.c
@@ -13,19 +13,19 @@ int main(void) {
     IMC* vetor[4];
 
     Altura *a0 = alt_cria(1,70);
-    Peso *p0 = peso_criar(47,500);
+    Peso *p0 = peso_cria(47,500);
     vetor[0] = imc_cria("Henrique", a0, p0);
 
     Altura *a1 = alt_cria(1,63);
-    Peso *p1 = peso_criar(54,300);
+    Peso *p1 = peso_cria(54,300);
     vetor[1] = imc_cria("Ana", a1, p1);
 
     Altura *a2 = alt_cria(1,73);
-    Peso *p2 = peso_criar(60,000);
+    Peso *p2 = peso_cria(60,000);
     vetor[2] = imc_cria("Matheus", a2, p2);
 
     Altura *a3 = alt_cria(1,85);
-    Peso *p3 = peso_criar(54,500);
+    Peso *p3 = peso_cria(54,500);
     vetor[3] = imc_cria("Manuelle", a3, p3);
 
     for (int i = 0; i < 4; i++) {
